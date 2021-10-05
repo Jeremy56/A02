bc3da6295d2a037bdc19bf74c0170d2e0d01ad90
Jeremy56 <shermansystemdown@gmail.com>
2021-10-04 20:51:17 -0500

    Added functionality for guessing game

--- App.java
+++ App.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class App {
+	public static void main(String[] args)
+    {
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Guess a number between 1 and 10");
+        
+        int secretNum = 7;
+        int playAgain = 0;
 
+        int guess = numbers.nextInt();
+
+        if (guess == secretNum) 
+        {
+        	System.out.println("You guessed the secret number!");
+        }else{
+        	System.out.println("You didnt guess the secret number :(");
+        }
+    }
 }
